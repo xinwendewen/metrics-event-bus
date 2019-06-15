ac0319a8593f557d783fccdf851eee8f396b3889
xinwendewen <zwt0909@outlook.com>
2019-06-15 22:32:45 +0800

    add project

---
+++ src/main/java/com/xinwendewen/metricseventbus/MetricsEventBusApplication.java
@@ -0,0 +1,13 @@
+package com.xinwendewen.metricseventbus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricsEventBusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetricsEventBusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xinwendewen/metricseventbus/MetricsEventBusApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xinwendewen.metricseventbus;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MetricsEventBusApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
